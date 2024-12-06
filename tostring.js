bd0baa467444f212a08e945018c9f667e59a19bb
Benly <benly.c@lezdotechmed.com>
2024-12-06 09:51:44 +0530

    tostring

---
+++ tostring.js
@@ -0,0 +1,6 @@
+// Javascript Array toString()
+// The toString() method returns a string formed by the elements of the given array.
+
+const fruits = ["Banana", "Orange", 245, 55, false];
+
+console.log("fruits", fruits.toString());
