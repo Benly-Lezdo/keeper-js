99943c78219e8c326998ec80db428335db22bb1d
Benly <benly.c@lezdotechmed.com>
2025-02-05 09:59:42 +0530

    changes

--- forEach.js
+++ forEach.js
@@ -3,3 +3,9 @@
 const val = ["a", "b", "c"];
 
 const vv = val.forEach((item) => console.log("item", item.toUpperCase()));
+
+const vvv = val.forEach((item) => {
+    console.log('iam', item);  // This will print each item to the console
+});
+
+console.log(vvv)
