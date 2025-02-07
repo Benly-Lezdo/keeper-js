a246092425f29633523ff12cc2c48876a2c9e4be
Benly <benly.c@lezdotechmed.com>
2025-02-07 08:37:44 +0530

    reverse

---
+++ reverse.js
@@ -0,0 +1,13 @@
+// reverse the array 
+
+// example 1
+const v = ['one', 'two', 'three'];
+
+const vv = [...v].reverse();
+
+console.log(vv)
+
+// example => 2
+const v1 = ['one', 'two', 'three'];
+
+const vvv = v1.reverse()
