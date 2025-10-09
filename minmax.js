f5b2a53d528086f0412456bcd38eea2217d7b326
Benly <benly.c@lezdotechmed.com>
2025-10-09 11:03:37 +0530

    min & max

--- minmax.js
+++ minmax.js
@@ -9,3 +9,25 @@ const vv = Math.max.apply(Math, b);
 
 console.log("v", v);
 console.log("vv", vv);
+
+// example => 1 remove the highest number
+const numbers = [1, 2, 3, 4, 100, 88, 77];
+
+// Find the highest number
+const max = Math.max(...numbers);
+
+// Remove it
+const result = numbers.filter(num => num !== max);
+
+console.log(result); // [1, 2, 3, 4, 88, 77]
+
+// example => 2 remove the lowest number
+const numbersList = [1, 2, 3, 4, 100, 88, 77];
+
+// Find the highest number
+const min = Math.min(...numbersList);
+
+// Remove it
+const resultNew = numbersList.filter(num => num !== min);
+
+console.log(resultNew); // [2, 3, 4, 100, 88, 77]
