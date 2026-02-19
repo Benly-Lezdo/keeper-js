a56e9fb414b22c31bfe766b0dfcaa69328b64ed6
Benly <benly.c@lezdotechmed.com>
2026-02-19 15:51:56 +0530

    ternary operator

---
+++ ternaryOperator.js
@@ -0,0 +1,25 @@
+// Ternary operator (?:) is a shorthand way to write an if-else condition in a single line.
+
+// example => 1
+const age = 20;
+const status = age >= 18 ? "Adult" : "Minor";
+
+console.log("status", status);
+
+// example =>2
+const isLoggedIn = true;
+const message = isLoggedIn ? "Welcome" : "Please login";
+
+console.log("message", message);
+
+// example =>3
+const num = 5;
+const result = num % 2 === 0 ? "Even" : "Odd";
+
+console.log("result", result);
+
+// example =>4
+const a = NaN;
+const b = a ? true : false;
+
+console.log("b", b);
