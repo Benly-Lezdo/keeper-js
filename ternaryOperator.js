d0b1883a07b3d97c2946782dc2ea189bca0bdc33
Benly <benly.c@lezdotechmed.com>
2026-02-19 15:54:42 +0530

    ternary operator

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -6,20 +6,26 @@ const status = age >= 18 ? "Adult" : "Minor";
 
 console.log("status", status);
 
-// example =>2
+// example => 2
 const isLoggedIn = true;
 const message = isLoggedIn ? "Welcome" : "Please login";
 
 console.log("message", message);
 
-// example =>3
+// example => 3
 const num = 5;
 const result = num % 2 === 0 ? "Even" : "Odd";
 
 console.log("result", result);
 
-// example =>4
+// example => 4
 const a = NaN;
 const b = a ? true : false;
 
 console.log("b", b);
+
+// example => 5
+const v = 1;
+const val = v == 1 ? true : false;
+
+console.log("val", val);
