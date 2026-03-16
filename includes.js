f8190c3f5d0b2bdc252e7d9bf519e9dbce9a4e90
Benly <benly.c@lezdotechmed.com>
2026-03-16 15:05:06 +0530

    includes search function check

--- includes.js
+++ includes.js
@@ -39,3 +39,72 @@ const filteredLinks1 = visibleProfileIds1.includes(profileId1)
   : links1;
 
 console.log("filteredLinks1", filteredLinks1);
+
+// example 3 =>
+// toLowerCase() converts a string to all lowercase letters so comparison becomes case-insensitive.
+// includes() checks if a string contains a specific word or character and returns true or false.
+
+import { useState } from "react";
+
+function App() {
+  const data = [
+    { id: 1, value: "blue" },
+    { id: 2, value: "red" },
+    { id: 3, value: "orange" },
+  ];
+
+  const [search, setSearch] = useState("");
+  const [show, setShow] = useState(false);
+
+  const filteredData = data.filter((item) =>
+    item.value.toLowerCase().includes(search.toLowerCase()),
+  );
+  console.log(filteredData);
+  return (
+    <div style={{ width: "200px", position: "relative" }}>
+      <input
+        type="text"
+        placeholder="Search color..."
+        value={search}
+        onClick={() => setShow(true)}
+        onChange={(e) => {
+          setSearch(e.target.value);
+          setShow(true);
+        }}
+      />
+
+      {show && (
+        <div style={{ border: "1px solid #ccc", background: "#fff" }}>
+          {filteredData.map((item) => (
+            <div
+              key={item.id}
+              style={{ padding: "5px", cursor: "pointer" }}
+              onClick={() => {
+                setSearch(item.value);
+                setShow(false);
+              }}
+            >
+              {item.value}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
+// example 4 =>
+const d = [
+  { id: 1, value: "blue" },
+  { id: 2, value: "red" },
+  { id: 3, value: "orange" },
+];
+
+const v = "o";
+
+const filteredData = d.filter((item) =>
+  item.value.toLowerCase().includes(v.toLowerCase()),
+);
+console.log(filteredData);
