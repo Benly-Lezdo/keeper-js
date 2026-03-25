15ece2308e953ca3ec3de774530302483cf944cb
Benly <benly.c@lezdotechmed.com>
2026-03-25 10:34:38 +0530

    changes

---
+++ keeper-py/f-strings.py
@@ -0,0 +1,22 @@
+# f-strings in python
+
+# example => 1
+name = 'Om'
+age = 22
+print(f"Hello, My name is {name} and I'm {age} years old.")
+
+# example => 2
+# Prints today's date with help
+# of datetime library
+
+import datetime
+
+today = datetime.datetime.today()
+print(f"{today:%B %d, %Y}")
+
+# example => 3
+english = 78
+maths = 56
+hindi = 85
+
+print(f"Ram got total marks {english + maths + hindi} out of 300")
